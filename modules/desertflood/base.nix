60925fc04adc193237d7bdaf7c6a61230c7700d9
Joe Martin <joe@desertflood.com>
2025-07-18 14:10:30 -0700

    arch: separate out step pki

--- modules/desertflood/base.nix
+++ modules/desertflood/base.nix
@@ -2,38 +2,11 @@
 let
   myTimezone = "America/Phoenix";
   myLocale = "en_US.UTF-8";
-  pkiRootCert =
-    pkgs:
-    pkgs.fetchurl {
-      url = "https://pki.desertflood.link/roots.pem";
-      hash = "sha256-tgK+D2q/bJBZCDh+7Cf9ha2wSzIfh7n2HZi5Vif6g1k=";
-      curlOpts = "--insecure";
-    };
-  stepConfig =
-    pkgs:
-    pkgs.writeTextFile {
-      name = "step-defaults.json";
-      text = builtins.toJSON {
-        ca-url = "https://pki.desertflood.link";
-        fingerprint = "be95020a50bc30002b6f5a2ea3cd827b169412235192adeb3296a827d0036e00";
-        root = "${pkiRootCert pkgs}";
-      };
-    };
-  step =
-    pkgs:
-    pkgs.writeShellApplication {
-      name = "step";
-      text = # bash
-        ''
-          ${pkgs.lib.getExe pkgs.step-cli} --config="${stepConfig pkgs}" "$@"
-        '';
-    };
   defaultPackages = pkgs: [
     pkgs.cacert
     pkgs.curl
     pkgs.ghostty.terminfo
     pkgs.gum
-    (step pkgs)
     pkgs.vim
     pkgs.wget
   ];
@@ -61,8 +34,6 @@ in
           MaxAuthTries 6
           MaxSessions 10
 
-          # Host certificate verification
-          HostCertificate /etc/ssh/ssh_host_ed25519_key-cert.pub
 
           # Environment
           AcceptEnv COLORTERM
@@ -93,10 +64,6 @@ in
         };
       };
 
-      security.pki.certificateFiles = [
-        (pkiRootCert pkgs)
-      ];
-
       nix = {
 
         registry.nixpkgs.to = {
@@ -121,67 +88,6 @@ in
             "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
             "df-test:nGcIjsIXVK/SZON7K4/IVCJPZ2PjNSRFyB2RBDHMPsU="
           ];
-        };
-      };
-
-      systemd = {
-
-        services = {
-          renew-ssh-certificate = {
-            name = "renew-ssh-certificate.service";
-            description = "Weekly renewal for SSH host certificate";
-
-            wants = [
-              "network.target"
-              "network-online.target"
-            ];
-
-            after = [
-              "network.target"
-              "network-online.target"
-            ];
-
-            serviceConfig = {
-              Type = "oneshot";
-              User = "root";
-            };
-
-            script = # bash
-              ''
-                ${pkgs.lib.getExe (step pkgs)} ssh renew "/etc/ssh/ssh_host_ed25519_key-cert.pub" "/etc/ssh/ssh_host_ed25519_key" --force 2> /dev/null
-                exit 0
-              '';
-
-          };
-        };
-
-        timers = {
-
-          renew-ssh-certificate = {
-            name = "renew-ssh-certificate.timer";
-            description = "Weekly renewal for SSH host certificate";
-            wantedBy = [ "timers.target" ];
-
-            timerConfig = {
-              # Run 5 minutes after system / systemd start
-              OnStartupSec = "5min";
-
-              # Run weekly
-              OnCalendar = "weekly";
-
-              # Delay the timer by a randomly selected, evenly distributed amount of time between
-              # 0 seconds and 30 minutes
-              RandomizedDelaySec = 1800;
-
-              # Coalesce CPU wakeups
-              AccuracySec = "1h";
-
-              # store the time when the service unit was last triggered
-              # When the timer is activated, trigger the service unit immediately
-              # if it would have been triggered at least while inactive
-              Persistent = true;
-            };
-          };
         };
       };
     };
---
+++ modules/desertflood/step-ssh.nix
@@ -0,0 +1,89 @@
+_: {
+
+  flake.modules.nixos.step-ssh =
+    { lib, pkgs, ... }:
+    {
+
+      environment.systemPackages = [
+        pkgs.local.signEm
+        pkgs.local.step
+      ];
+
+      services = {
+
+        openssh = {
+          extraConfig = ''
+            # Host certificate verification
+            HostCertificate /etc/ssh/ssh_host_ed25519_key-cert.pub
+            HostCertificate /etc/ssh/ssh_host_rsa_key-cert.pub
+          '';
+        };
+
+      };
+
+      security.pki.certificateFiles = [ pkgs.local.pkiRootCert ];
+
+      systemd = {
+        services.renew-ssh-certificate = {
+          name = "renew-ssh-certificate.service";
+          description = "Weekly renewal for SSH host certificate";
+
+          wants = [
+            "network.target"
+            "network-online.target"
+          ];
+
+          after = [
+            "network.target"
+            "network-online.target"
+          ];
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+
+          script = # bash
+            ''
+              if [[ -f "/etc/ssh/ssh_host_rsa_key-cert.pub" ]];
+              then
+                ${lib.getExe pkgs.local.step} ssh renew "/etc/ssh/ssh_host_rsa_key-cert.pub" "/etc/ssh/ssh_host_rsa_key" --force 2> /dev/null
+              fi
+              if [[ -f "/etc/ssh/ssh_host_ed25519_key-cert.pub" ]];
+              then
+                ${lib.getExe pkgs.local.step} ssh renew "/etc/ssh/ssh_host_ed25519_key-cert.pub" "/etc/ssh/ssh_host_ed25519_key" --force 2> /dev/null
+              fi
+              exit 0
+            '';
+        };
+
+        timers.renew-ssh-certificate = {
+          name = "renew-ssh-certificate.timer";
+          description = "Weekly renewal for SSH host certificate";
+          wantedBy = [ "timers.target" ];
+
+          timerConfig = {
+            # Run 5 minutes after system / systemd start
+            OnStartupSec = "5min";
+
+            # Run weekly
+            OnCalendar = "weekly";
+
+            # Delay the timer by a randomly selected, evenly distributed amount of time between
+            # 0 seconds and 30 minutes
+            RandomizedDelaySec = 1800;
+
+            # Coalesce CPU wakeups
+            AccuracySec = "1h";
+
+            # store the time when the service unit was last triggered
+            # When the timer is activated, trigger the service unit immediately
+            # if it would have been triggered at least while inactive
+            Persistent = true;
+          };
+        };
+      };
+
+    };
+
+}
