98bf8cb17216a58982899ce10213a1417c1af443
Joe Martin <joe@desertflood.com>
2025-07-21 15:37:13 -0700

    feat: local overlay two ways

--- modules/flake/nixpkgs.nix
+++ modules/flake/nixpkgs.nix
@@ -6,6 +6,19 @@ let
 in
 {
   config = {
+    perSystem =
+      { system, config, ... }:
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              local = config.packages;
+            })
+          ];
+        };
+      };
+
     flake.modules.nixos.nixos = {
       nixpkgs.overlays = [
         overlay
