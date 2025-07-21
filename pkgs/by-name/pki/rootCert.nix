dd21fda41d7084c20a75ac6adff156ccbdab8ea0
Joe Martin <joe@desertflood.com>
2025-07-21 15:41:39 -0700

    fix: pki/rootCert derivation broken

--- pkgs/by-name/pki/rootCert.nix
+++ pkgs/by-name/pki/rootCert.nix
@@ -1,10 +1,10 @@
 {
   fetchurl,
-  stdenv,
+  writeTextFile,
 }:
-stdenv.mkDerivation {
-  name = "pkiRootCert";
-  src = fetchurl {
+writeTextFile {
+  name = "pkiroots.pem";
+  text = fetchurl {
     url = "https://pki.desertflood.link/roots.pem";
     hash = "sha256-tgK+D2q/bJBZCDh+7Cf9ha2wSzIfh7n2HZi5Vif6g1k=";
     curlOpts = "--insecure";
