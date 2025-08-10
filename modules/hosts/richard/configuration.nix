481a17c23900890e00bb9baf5d3c551ddb2ba1af
Joe Martin <joe@desertflood.com>
2025-08-09 23:21:40 -0700

    refactor(richard): use config for both flake-parts and Nix OS modules

--- modules/hosts/richard/configuration.nix
+++ modules/hosts/richard/configuration.nix
@@ -8,42 +8,50 @@
 let
   defaultUser = config.desertflood.users.users.joe;
   hostInfo = config.desertflood.hosts.hosts.richard;
+  flakeCfg = config;
 in
 {
-  flake.modules.nixos.richard = {
-    imports = [
-      inputs.self.modules.nixos.base
-      inputs.self.modules.nixos.base-server
-      inputs.self.modules.nixos.remote-builder
-      inputs.self.modules.nixos.proxmox-lxc
-    ];
+  flake.modules.nixos.richard =
+    { config, ... }:
+    let
+      cfg = config;
+    in
+    {
+      imports = [
+        inputs.self.modules.nixos.base
+        inputs.self.modules.nixos.base-server
+        inputs.self.modules.nixos.remote-builder
+        inputs.self.modules.nixos.proxmox-lxc
+      ];
 
-    desertflood.defaultUser = defaultUser;
-    desertflood.hostInfo = hostInfo;
+      desertflood.defaultUser = defaultUser;
+      desertflood.hostInfo = hostInfo;
 
-    networking = {
-      inherit (hostInfo) hostName domain;
+      networking = {
+        inherit (hostInfo) hostName domain;
+      };
+      age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKf0pQkV2GuvDHvX0OFyVKDDmizEbW5nfJJz7Xms2KYr";
+
+      };
+
+      nix.settings.trusted-users = [ "nixos" ];
+
+      users.users = {
+
+        nixos = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+          openssh.authorizedKeys.keys = defaultUser.authorizedKeys;
+        };
+
+        root = {
+          openssh.authorizedKeys.keys = defaultUser.authorizedKeys;
+        };
+
+        builder = {
+          openssh.authorizedKeys.keys = [ flakeCfg.desertflood.builderKeys.builderKeys.psyche.publicKey ];
+        };
+
+      };
     };
-    age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKf0pQkV2GuvDHvX0OFyVKDDmizEbW5nfJJz7Xms2KYr";
-
-    nix.settings.trusted-users = [ "nixos" ];
-
-    users.users = {
-
-      nixos = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        openssh.authorizedKeys.keys = defaultUser.authorizedKeys;
-      };
-
-      root = {
-        openssh.authorizedKeys.keys = defaultUser.authorizedKeys;
-      };
-
-      builder = {
-        openssh.authorizedKeys.keys = [ config.desertflood.builderKeys.builderKeys.psyche.publicKey ];
-      };
-
-    };
-  };
 }
