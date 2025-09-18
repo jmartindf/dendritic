2fac85dbb76453447d2095fdc5499fa362661bbf
Joe Martin <joe@desertflood.com>
2025-09-17 20:23:28 -0700

    feat: ntfy 2.14.0 and dendritic module

---
+++ modules/desertflood/services/ntfy/default.nix
@@ -0,0 +1,60 @@
+_: {
+
+  config = {
+
+    flake.modules.nixos.ntfy =
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      let
+        nixOScfg = config;
+      in
+      {
+
+        options = {
+          desertflood.services.ntfy = {
+            enable = lib.mkEnableOption "serving ntfy, a simple HTTP-based pub-sub notification service";
+          };
+        };
+
+        config = lib.mkIf nixOScfg.desertflood.services.ntfy.enable {
+
+          age.secrets.ntfy-sh-secrets = {
+            rekeyFile = ./secrets.env.age;
+            owner = "ntfy-sh";
+            group = "ntfy-sh";
+          };
+
+          desertflood.networking.services.ntfy = { };
+
+          services.ntfy-sh =
+            let
+              svcConfig = nixOScfg.desertflood.networking.services.ntfy;
+            in
+            {
+              enable = true;
+
+              package = pkgs.local.ntfy-sh;
+
+              settings = {
+                base-url = "${svcConfig.fullURL}";
+                behind-proxy = true;
+                auth-default-access = "deny-all";
+                upstream-base-url = "https://ntfy.sh";
+              };
+            };
+
+          systemd.services.ntfy-sh = {
+            serviceConfig.EnvironmentFile = nixOScfg.age.secrets.ntfy-sh-secrets.path;
+          };
+
+        };
+
+      };
+
+  };
+
+}
