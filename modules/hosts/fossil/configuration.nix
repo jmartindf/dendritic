3d60d91d1e99ba4441e9566bae93cbd17acc26d9
Joe Martin <joe@desertflood.com>
2025-09-21 14:43:25 -0700

    style(fossil): tweak services code

--- modules/hosts/fossil/configuration.nix
+++ modules/hosts/fossil/configuration.nix
@@ -43,71 +43,59 @@ in
 
         step-ca.certs.${hostName}.availableTo = { };
 
-        services.caddy = {
-          enable = true;
-
-          letsencrypt = {
+        services = {
+          caddy = {
             enable = true;
-            acme-dns = true;
-          };
 
-          settings = {
-            site-blocks = # caddy
-              ''
-                ${webHost} {
-                  reverse_proxy /grafana* http://${toString svcConfig.grafana.settings.server.http_addr}:${toString svcConfig.grafana.settings.server.http_port}
-                  reverse_proxy /prometheus* https://${toString svcConfig.prometheus.listenAddress}:${toString svcConfig.prometheus.port}
+            letsencrypt = {
+              enable = true;
+              acme-dns = true;
+            };
 
-                  handle_path /forgejo* {
-                    request_body {
-                      max_size 1G
-                    }
-                    reverse_proxy http://${toString svcConfig.forgejo.settings.server.HTTP_ADDR}:${toString svcConfig.forgejo.settings.server.HTTP_PORT}
-                  }
-
-                  handle_path /taskchamp* {
-                    reverse_proxy http://${toString svcConfig.taskchampion-sync-server.host}:${toString svcConfig.taskchampion-sync-server.port}
-                  }
-                }
-
-                *.desertflood.com {
-                  import challenge_dns_acme-dns
-
-                  @monitor host monitor.desertflood.com
-                  handle @monitor {
+            settings = {
+              site-blocks = # caddy
+                ''
+                  ${webHost} {
                     reverse_proxy /grafana* http://${toString svcConfig.grafana.settings.server.http_addr}:${toString svcConfig.grafana.settings.server.http_port}
                     reverse_proxy /prometheus* https://${toString svcConfig.prometheus.listenAddress}:${toString svcConfig.prometheus.port}
-                  }
 
-                  handle {
-                    abort
+                    handle_path /forgejo* {
+                      request_body {
+                        max_size 1G
+                      }
+                      reverse_proxy http://${toString svcConfig.forgejo.settings.server.HTTP_ADDR}:${toString svcConfig.forgejo.settings.server.HTTP_PORT}
+                    }
+
+                    handle_path /taskchamp* {
+                      reverse_proxy http://${toString svcConfig.taskchampion-sync-server.host}:${toString svcConfig.taskchampion-sync-server.port}
+                    }
                   }
-                }
-              '';
+                '';
+            };
+
           };
 
-        };
+          prometheus = {
+            inherit mTLS-required;
 
-        services.prometheus = {
-          inherit mTLS-required;
+            exporters.node.mTLS-required = mTLS-required;
 
-          exporters.node.mTLS-required = mTLS-required;
+            monitorHosts = [
+              "everest.manticore-mark.ts.net"
+              "firewalla.manticore-mark.ts.net"
+              "hermes.manticore-mark.ts.net"
+              "mark.manticore-mark.ts.net"
+              "masto-es.manticore-mark.ts.net"
+              "mastodon.manticore-mark.ts.net"
+              "underworld.manticore-mark.ts.net"
+            ];
 
-          monitorHosts = [
-            "everest.manticore-mark.ts.net"
-            "firewalla.manticore-mark.ts.net"
-            "hermes.manticore-mark.ts.net"
-            "mark.manticore-mark.ts.net"
-            "masto-es.manticore-mark.ts.net"
-            "mastodon.manticore-mark.ts.net"
-            "underworld.manticore-mark.ts.net"
-          ];
-
-          monitorHostsSecure = [
-            "127.0.0.1"
-            "richard.manticore-mark.ts.net"
-            "france.manticore-mark.ts.net"
-          ];
+            monitorHostsSecure = [
+              "127.0.0.1"
+              "richard.manticore-mark.ts.net"
+              "france.manticore-mark.ts.net"
+            ];
+          };
         };
       };
 
