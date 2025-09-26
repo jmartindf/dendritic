28a750afccfba5943acb949304cfe90d49b71d22
Joe Martin <joe@desertflood.com>
2025-09-26 16:47:08 -0700

    feat(flake): pump up default devshell

--- modules/flake/flake.nix
+++ modules/flake/flake.nix
@@ -14,8 +14,37 @@
       { config, pkgs, ... }:
       {
         # configure the flake's dev shell
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ config.agenix-rekey.package ];
+        devshells.default = {
+
+          packages = [
+            pkgs.just
+            pkgs.nix
+            pkgs.nixos-rebuild-ng
+            pkgs.nix-output-monitor
+          ];
+
+          commands = [
+            {
+              package = pkgs.nh;
+              help = "Nix helper for nixpkgs development";
+            }
+            {
+              package = pkgs.nix-tree;
+              help = "Interactively browse dependency graphs of Nix derivations";
+            }
+            {
+              package = pkgs.nvd;
+              help = "Diff two nix toplevels and show which packages were upgraded";
+            }
+            {
+              package = pkgs.nix-diff;
+              help = "Explain why two Nix derivations differ";
+            }
+            {
+              package = pkgs.nix-output-monitor;
+              help = "Nix Output Monitor (a drop-in alternative for `nix` which shows a build graph)";
+            }
+          ];
         };
 
       };
