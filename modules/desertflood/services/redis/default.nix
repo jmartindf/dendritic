3a9d009f72517858b9333aa7ea714544eac09788
Joe Martin <joe@desertflood.com>
2025-09-26 21:09:56 -0700

    feat(redis): new service module

---
+++ modules/desertflood/services/redis/default.nix
@@ -0,0 +1,45 @@
+_: {
+  # SPDX-FileCopyrightText: 2025 Joe Martin <joe@desertflood.com>
+  # SPDX-License-Identifier: BlueOak-1.0.0
+
+  config = {
+
+    flake.modules.nixos.redis =
+      {
+        config,
+        lib,
+        pkgs,
+        ...
+      }:
+      let
+        nixOScfg = config;
+      in
+      {
+        imports = [ ];
+
+        options = {
+
+          desertflood.services.redis = {
+            enable = lib.mkEnableOption {
+              default = false;
+              description = "Redis caching server";
+            };
+          };
+
+        };
+
+        config = lib.mkIf nixOScfg.desertflood.services.redis.enable {
+
+          age.secrets.redis-password.rekeyFile = ./redis-password.age;
+
+          services.redis.servers."" = {
+            enable = true;
+            bind = "127.0.0.1";
+            requirePassFile = nixOScfg.age.secrets.redis-password.path;
+          };
+
+        };
+      };
+
+  };
+}
