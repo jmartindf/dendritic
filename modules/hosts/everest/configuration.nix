ed2c082eaec61b6d5e2146594dfee8de1e2b2e0c
Joe Martin <joe@desertflood.com>
2025-10-16 15:55:20 -0700

    feat(everest): deploy nix-managed authelia and traefik

--- modules/hosts/everest/configuration.nix
+++ modules/hosts/everest/configuration.nix
@@ -37,6 +37,7 @@ in
         inputs.self.modules.nixos.dockeras
         inputs.self.modules.nixos.lldap
         inputs.self.modules.nixos.step-ca
+        inputs.self.modules.nixos.authelia
       ];
 
       desertflood = {
@@ -44,6 +45,16 @@ in
 
         networking = {
           webDomain = "df.fyi";
+
+          services = {
+            lldap = {
+              protocol = "https";
+              domain = "desertflood.link";
+              hostName = "lldap";
+              path = "";
+            };
+          };
+
         };
 
         step-ca = {
@@ -59,6 +70,63 @@ in
             # run ca-server
             enable = true;
             fqdns = [ "pki.desertflood.link" ];
+          };
+
+          authelia.enable = true;
+
+          traefik = {
+            enable = true;
+
+            domain = nixOScfg.desertflood.networking.tsFQDN;
+
+            letsencrypt = {
+              enable = true;
+              tailscaleCerts = true;
+              production = true;
+              defaultResolver = "tailscale";
+              acme-dns = true;
+            };
+
+            rules = {
+
+              smallstep.tcp = {
+                routers.smallstep-rtr = {
+                  entrypoints = "websecure";
+                  service = "smallstep-svc";
+                  tls.passthrough = true;
+                  rule = "HostSNI(`pki.desertflood.link`)";
+                };
+
+                services.smallstep-svc.loadbalancer.servers = [ { address = "127.0.0.1:8443"; } ];
+              };
+
+              lldap.http = {
+
+                routers.lldap-rtr = {
+                  entrypoints = "websecure";
+                  tls.certresolver = "web";
+                  rule = "Host(`lldap.desertflood.link`)";
+                  middlewares = "chain-no-auth@file";
+                  service = "lldap-svc";
+                };
+
+                services.lldap-svc.loadbalancer.servers = [ { url = "http://127.0.0.1:17170"; } ];
+              };
+
+              authelia.http = {
+
+                routers.authelia-rtr = {
+                  entrypoints = "websecure";
+                  tls.certresolver = "web";
+                  rule = "Host(`idm.thosemartins.family`) || Host(`idm.desertflood.link`) || Host(`idm.df.fyi`)";
+                  middlewares = "chain-no-auth@file";
+                  service = "authelia-svc";
+                };
+
+                services.authelia-svc.loadbalancer.servers = [ { url = "http://127.0.0.1:9091"; } ];
+              };
+            };
+
           };
         };
       };
