690fb273e3c2b6f49ef65fe0751e7e3494847783
Joe Martin <joe@desertflood.com>
2025-10-17 16:13:35 -0700

    feat(grafana): enable OIDC

--- modules/desertflood/services/grafana/default.nix
+++ modules/desertflood/services/grafana/default.nix
@@ -1,4 +1,4 @@
-{ config, inputs, ... }:
+{ config, ... }:
 let
   flakeCfg = config;
 in
@@ -18,11 +18,6 @@ in
 
       config = {
 
-        age.secrets.grafana-password = {
-          rekeyFile = ./grafana-password.age;
-          owner = grafanaUser;
-          group = grafanaGroup;
-        };
 
         desertflood.services.postgresql.enable = true;
         desertflood.networking.services.grafana = { };
@@ -32,6 +27,18 @@ in
           postgresql = {
             ensureDatabases = [ grafanaUser ];
             ensureUsers = [
+          age.secrets = {
+            grafana-password = {
+              rekeyFile = ./grafana-password.age;
+              owner = grafanaUser;
+              group = grafanaGroup;
+            };
+            grafana-client-secret = {
+              rekeyFile = ./grafana-client-secret.age;
+              owner = grafanaUser;
+              group = grafanaGroup;
+            };
+          };
               {
                 name = grafanaUser;
                 ensureDBOwnership = true;
@@ -62,12 +69,6 @@ in
                   user = grafanaUser;
                 };
 
-                security = {
-                  admin_user = "jmartindf";
-                  admin_password = "$__file{${cfg.age.secrets.grafana-password.path}}";
-                  admin_email = flakeCfg.desertflood.defaultUser.emails.family.email;
-                  cookie_secure = true;
-                };
               };
 
               provision = {
@@ -110,6 +111,42 @@ in
         }; # end `services` block
 
       }; # end Nix OS module config block
+                  security = {
+                    admin_user = "gfadmin";
+                    admin_password = "$__file{${cfg.age.secrets.grafana-password.path}}";
+                    admin_email = flakeCfg.desertflood.defaultUser.emails.family.email;
+                    cookie_secure = true;
+                  };
+
+                  auth = {
+                    oauth_allow_insecure_email_lookup = false; # True to match existing users by email address
+                  };
+
+                  "auth.generic_oauth" = {
+                    enabled = true;
+                    auto_login = true;
+                    allow_sign_up = true;
+                    use_refresh_token = true;
+                    allow_assign_grafana_admin = true;
+                    name = "Authentik Desertflood";
+                    client_id = "szq3K7BWLSsPDkyRpw7AnxPbroFXoT6pKhdbVYSX";
+                    client_secret = "$__file{${cfg.age.secrets.grafana-client-secret.path}}";
+                    scopes = [
+                      "openid"
+                      "email"
+                      "profile"
+                      "offline_access"
+                    ];
+                    auth_url = "https://sso.desertflood.link/application/o/authorize/";
+                    token_url = "https://sso.desertflood.link/application/o/token/";
+                    api_url = "https://sso.desertflood.link/application/o/userinfo/";
+                    signout_redirect_url = "https://sso.desertflood.link/application/o/grafana/end-session/";
+                    role_attribute_path = "contains(groups[*], 'Grafana Server Admins') && 'GrafanaAdmin' || contains(groups[*], 'Grafana Admins') && 'Admin' || contains(groups[*], 'Grafana Editors') && 'Editor' || 'Viewer'";
+                    login_attribute_path = "preferred_username";
+                    name_attribute_path = "given_name";
+                    email_attribute_path = "email";
+                    email_attribute_name = "email";
+                  };
 
     }; # end `grafana` Nix OS module
 }
