24d5ee8908fc5d47a31a0de93faa9cf923ad3a36
Joe Martin <joe@desertflood.com>
2026-02-07 15:00:54 -0700

    feat(home-manager): auto backup and overwrite existing files

---
+++ modules/desertflood/den/home-manager.nix
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: 2025 Joe Martin <joe@desertflood.com>
+# SPDX-License-Identifier: BlueOak-1.0.0
+{
+  den,
+  inputs,
+  ...
+}:
+let
+  osContext = den.lib.take.exactly (
+    { OS, host }:
+    {
+      ${host.class} = {
+        config = {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "backup";
+            overwriteBackup = true;
+          };
+        };
+      };
+    }
+  );
+
+  homeContext = den.lib.take.exactly (
+    {
+      OS,
+      host,
+      user,
+    }:
+    {
+      homeManager =
+        builtins.traceVerbose "configuring home-manager.enable for ${user.userName}@${host.name}"
+          {
+            # Let Home Manager install and manage itself.
+            programs.home-manager.enable = true;
+          };
+    }
+  );
+in
+{
+
+  imports = [ inputs.home-manager.flakeModules.home-manager ];
+
+  config = {
+
+    df.base.includes = [
+      osContext
+      homeContext
+    ];
+
+    flake-file.inputs.home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+}
