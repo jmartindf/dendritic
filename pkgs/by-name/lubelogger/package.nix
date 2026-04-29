aadb00cf6ac28c354c70b693c53b2d4c5815d8a7
Joe Martin <joe@desertflood.com>
2026-04-29 13:52:11 -0700

    lubelogger: 1.6.3 -> 1.6.4
    Diff: https://github.com/hargata/lubelog/compare/v1.6.3...v1.6.4
    
    Changelog: https://github.com/hargata/lubelog/releases/tag/v1.6.4

--- pkgs/by-name/lubelogger/package.nix
+++ pkgs/by-name/lubelogger/package.nix
@@ -7,13 +7,13 @@
 
 buildDotnetModule rec {
   pname = "lubelogger";
-  version = "1.6.3";
+  version = "1.6.4";
 
   src = fetchFromGitHub {
     owner = "hargata";
     repo = "lubelog";
     rev = "v${version}";
-    hash = "sha256-cIEbFNPpEnKryN5Dyf/bCcrngLJxxftCwTQHv07W4AQ=";
+    hash = "sha256-w1UxnmuMBPi5Ov+3h7R0I0EIiZShsZm+TgXmfKdc1BU=";
   };
 
   projectFile = "CarCareTracker.sln";
